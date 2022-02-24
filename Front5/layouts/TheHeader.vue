ef6832d3123fde6b9c684f4203023bd5fd4de66b
Your Name <you@example.com>
2022-02-24 10:54:06 +0900

    フロントのトップページの作成

---
+++ Front5/layouts/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <div class="header-logo">
+      <h1>デモサイト</h1>
+    </div>    
+    <div class="header-list">
+      <ul>
+        <li>新規登録</li>
+        <li>ログイン</li>
+        <li>ログアウト</li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
